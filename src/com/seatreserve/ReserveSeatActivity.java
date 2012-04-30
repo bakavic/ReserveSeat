68fcb139d7bdaab912af45da03924e8f27a117f8
Victor Heng <bakavic@gmail.com>
2012-04-30 14:08:24 +0800

    initial commit

---
+++ src/com/seatreserve/ReserveSeatActivity.java
@@ -0,0 +1,13 @@
+package com.seatreserve;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReserveSeatActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
